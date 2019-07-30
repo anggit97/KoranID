391fab8967206c7f057459deb3765af572ab2ff1
Anggit Prayogo <anggitprayogo@gmail.com>
2019-07-30 10:51:10 +0700

    #Menambahkan Module Sport

--- app/src/main/java/com/bluparse/koranid/ui/MainActivity.kt
+++ app/src/main/java/com/bluparse/koranid/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.bluparse.koranid.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract.Directory.PACKAGE_NAME
 import android.util.Log.e
 import com.bluparse.core.base.BaseActivity
 import com.bluparse.koranid.R
 import com.bluparse.koranid.data.entity.TopHeadline
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+
+private const val SPORT_SAMPLE_CLASSNAME = "features_sport.SportActivity"
 
 class MainActivity : BaseActivity(), MainContact.View {
 
@@ -20,6 +25,10 @@ class MainActivity : BaseActivity(), MainContact.View {
         initPresenter()
 
         presenter.getTopHeadline("id", "sports")
+
+        tv_hello_world.setOnClickListener {
+            launchActivity(SPORT_SAMPLE_CLASSNAME)
+        }
     }
 
     private fun initPresenter() {
@@ -32,6 +41,13 @@ class MainActivity : BaseActivity(), MainContact.View {
 
     override fun showError(throwable: Throwable) {
         e("DATA ERROR: ", throwable.message)
+    }
+
+    /** Launch an activity by its class name. */
+    private fun launchActivity(className: String) {
+        val uri = Uri.parse("koranid://sport")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
     }
 
     override fun onDestroy() {
---
+++ features_sport/src/main/java/com/bluparse/features_sport/SportActivity.kt
@@ -0,0 +1,12 @@
+package com.bluparse.features_sport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SportActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sport)
+    }
+}
