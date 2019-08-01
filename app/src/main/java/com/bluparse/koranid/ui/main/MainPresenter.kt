2288694196d794c3614be537398878fb32a486d4
Anggit Prayogo <anggitprayogo@gmail.com>
2019-08-01 13:26:46 +0700

    #Menambahkan Loading pada main activity

--- app/src/main/java/com/bluparse/koranid/ui/main/MainPresenter.kt
+++ app/src/main/java/com/bluparse/koranid/ui/main/MainPresenter.kt
@@ -9,19 +9,22 @@ import javax.inject.Inject
  * Created by Anggit Prayogo on 2019-07-30.
  */
 class MainPresenter @Inject constructor(
-    val appDataManager: AppDataManager,
-    val appScheduler: AppSchedulerProvider
+    private val appDataManager: AppDataManager,
+    private val appScheduler: AppSchedulerProvider
 ) : BasePresenter<MainContact.View>(), MainContact.Presenter {
 
     override fun getTopHeadline(country: String, category: String) {
+        view?.showLoading()
         compositeDisposable.addAll(
             appDataManager.getTopHeadline(country, category)
                 .subscribeOn(appScheduler.io())
                 .observeOn(appScheduler.mainThread())
                 .subscribe(
                     { response ->
+                        view?.hideLoading()
                         view?.showResponseSuccess(response)
                     }, { error ->
+                        view?.hideLoading()
                         view?.showError(error)
                     }
                 )
